30605c30cd383c228dd7e0755e657ede3474e276
SteveMiller7 <info@millercreativedesign.com>
2022-05-27 09:26:31 +0000

    deleted original set up and started again. installed supporting libraries, started project and started app

--- hannahs/urls.py
+++ hannahs/urls.py
@@ -1,4 +1,4 @@
-"""hannah_booking URL Configuration
+"""hannahs URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- hannahs/wsgi.py
+++ hannahs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hannah_booking project.
+WSGI config for hannahs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hannah_booking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hannahs.settings')
 
 application = get_wsgi_application()
